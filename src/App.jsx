8b924d0e40fbeb40dd1dd014e55edbfdeb416e1d
rksh1995 <rakeshsuman1920@gmail.com>
2023-06-20 09:40:02 +0530

    add card,home,pagination

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Pagination from './Pagination'
+import "./App.css";
+import Pagination from "./Pagination";
+import Home from "./Home";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-  <div>
-    <Pagination/>
-  </div>
-  )
+    <div>
+      <Home />
+      <Pagination />
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Card/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './style.css';
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <img className="card-img" src={props.download_url} alt="cat" loading="lazy"/>
+      <h2 className="card-text">{props.author}</h2>
+      <p className="card-price">{props.width}</p>
+      <p className="card-price">{props.height}</p>
+      <button className="card-btn">Add to Cart</button>
+    </div>
+  );
+};
+
+export default Card;
